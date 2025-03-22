e91bdf21123f7f18f38ce99fa9397402bdc4f176
Josiah Michina <61215484+josmich100@users.noreply.github.com>
2025-03-22 08:52:16 +0300

    sign up and home page

--- app/src/main/java/com/kbyai/facerecognition/LoginActivity.kt
+++ app/src/main/java/com/kbyai/facerecognition/LoginActivity.kt
@@ -19,7 +19,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login_activity)
+        setContentView(R.layout.activity_login)
 
         editEmail = findViewById(R.id.editEmail)
         editPassword = findViewById(R.id.editPassword)
